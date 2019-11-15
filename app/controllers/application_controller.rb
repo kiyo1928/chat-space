9e062598534b308aaab9cf5a446f5222658823f2
kiyo1928 <fiveminute1218@gmail.com>
2019-11-15 20:49:36 +0900

    Add-user-management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # deviseで名前も保存できるようにする                deviseに関連する画面に移動した時に？という意味
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
+# devise_parameter_sanitizer.permit ストロングパラメーターの指定の仕方
