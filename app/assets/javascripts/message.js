eacd93aaacd6ec9a9c23ead6b694f55c78628a02
kiyo1928 <fiveminute1218@gmail.com>
2019-12-04 14:25:25 +0900

    add-Automatic-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){
   function buildHTML(message){
-   
     // カリキュラムに乗っている条件分岐はimageの有無の内容を大きく条件分岐したもの
     // 上記の理由からimageを司る表記をif文で条件分岐して変数に入れてからその変数を使って画像の表記をする
     // 上が画像ありの表記で下が画像なしの表記
@@ -12,13 +11,11 @@ $(function(){
 
     // 上記の条件文を三項演算子を利用した場合
      var image = message.image ? `<img src= "${message.image}" >`: ""
-    
-     
-
-    
+    // dat-idが反映されるようにしている
+    //  if (message.content && message.image) {
       // shift + @の記法でクォーテーションをつけるのを忘れずに
       // 上からメッセージ名前の要素、 投降日の要素、投降内容の要素、投降画像の要素
-      var html = `<div class="contents">
+      var html = `<div class="contents" data-message-id=${message.id}>
       <div class="contents__name">
       ${message.nickname}
       </div>
@@ -36,18 +33,12 @@ $(function(){
 
       </div>
       </div>`
+      
 
       return html;
-                }
+  }
     
-
-                
-                
-
-    
-    
-
-    // class =new_messageのsubmit(sendボタン)を実装
+     // class =new_messageのsubmit(sendボタン)を実装
   $('#new_message').on('submit',function(e){
     
     // htmlの機能の無効化
@@ -89,6 +80,58 @@ $(function(){
       // 非同期通信失敗時
     .fail(function(){
         alert("メッセージ送信に失敗しました");
+
     });
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    //  div要素contentsか最新(last())のメッセージを取得して、さらにその中からmessage-idを取得
+    last_message_id = $('.contents').last().data('message-id')
+    // groupのidを取得するためにdiv要素.header__left__groupからgroup-idの要素を取得
+    group_id = $('.header__left__group').last().data('group-id')
+    
+  
+   
+   
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+
+      url: `/groups/${group_id}/api/messages`,
+      
+
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 追加するhtmlの入れ物を作る
+      
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.main_contents').append(insertHTML);
+      $('.main_contents').animate({ scrollTop: $('.main_contents')[0].scrollHeight});
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+    
+  }; 
+ 
+    // setIntervalのエラーが出ないように対策
+    // チャットのグループ以外ではfailの時のアラートが出ないようにする
+    group_id = $('.header__left__group').last().data('group-id')
+  if(location.pathname == `/groups/${group_id}/messages`){
+    // 7秒ごとにリクエストをするように実装
+  setInterval(reloadMessages, 7000);
+ }
+  
+
 });
